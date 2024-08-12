a84d05fb40c98fd1634f23990d4a9f425dadacc3
Zach Privett <z.privett92@gmail.com>
2024-08-12 15:53:35 -0400

    added counter function

---
+++ frontend/azure-resume-app/src/components/Hero.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+  
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      
+      </div>
+      <div className='flex justify-center relative my-20 z-10'>
+        <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+          <h2 className='uppercase tracking-widest text-center text-xs text-blue-100 max-w-80'>
+            Hello
+          </h2>
+
+          <TextGenerateEffect 
+            className='text-center text-[40px] md:text-5xl lg:text-6xl'
+            words='Hello, My Name is Zachary Privett'
+          />
+
+          <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+            .Net Developer
+          </p>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Hero
---
+++ frontend/azure-resume-app/src/components/ui/BentoGrid.tsx
@@ -0,0 +1,85 @@
+import { cn } from "@/lib/utils";
+
+export const BentoGrid = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoGridItem = ({
+  className,
+  title,
+  description,
+  id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
+}: {
+  className?: string;
+  title?: string | React.ReactNode;
+  description?: string | React.ReactNode;
+  header?: React.ReactNode;
+  icon?: React.ReactNode;
+  id?: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
+
+}) => {
+  return (
+    <div
+      className={cn(
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        className
+      )}
+      style={{
+        background: 'rgb(2,0,36)',
+        backgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(109,109,207,1) 35%, rgba(0,212,255,1) 100%)' 
+      }}
+    >
+      <div className={`${id === 6} && flex justify-center h-full`}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, 'object-cover, object-center')}
+            />
+          )}
+        </div>
+        <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}> 
+          {spareImg && (
+            <img
+            src={spareImg}
+            alt={spareImg}
+            className={cn(imgClassName, 'object-cover, object-center w-full h-full')}
+          />           
+          )}
+        </div>
+      </div>
+      <div className="group-hover/bento:translate-x-2 transition duration-200">
+        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+          {title}
+        </div>
+        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+          {description}
+        </div>
+      </div>
+    </div>
+  );
+};
